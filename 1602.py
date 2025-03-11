127003d53b0a8e3017e7b8a6a92ed40e2cb2e68d
rairashmi <rashmi.rai050@gmail.com>
2025-03-11 16:08:32 +0530

    Add files via upload

---
+++ 1602.py
@@ -0,0 +1,118 @@
+Python 3.12.2 (tags/v3.12.2:6abddd9, Feb  6 2024, 21:26:36) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+2+3
+5
+9-1
+8
+9-8
+1
+4*6
+24
+8/4
+2.0
+5/2
+2.5
+5 // 2
+2
+8 + 9 - 10
+7
+8 + 9 -
+SyntaxError: invalid syntax
+8 + 2 * 3
+14
+(8 + 2) * 3
+30
+2 * 2 * 2
+8
+2**3
+8
+10 / 3
+3.3333333333333335
+10 // 3
+3
+10 % 3
+1
+'Rashmi'
+'Rashmi'
+print('Rashmi')
+Rashmi
+print ('Rashmi's laptop')
+       
+SyntaxError: unterminated string literal (detected at line 1)
+
+SyntaxError: unterminated string literal (detected at line 1)
+       
+SyntaxError: invalid syntax
+
+
+
+
+print ("Rashmi's Laptop")
+       
+Rashmi's Laptop
+Rashmi's Laptop
+       
+SyntaxError: unterminated string literal (detected at line 1)
+print('Rashmi "Laptop"')
+       
+Rashmi "Laptop"
+print('Rashmi's "Laptop"')
+      
+SyntaxError: unterminated string literal (detected at line 1)
+print ('Rashmi\'s "Laptop"')
+      
+Rashmi's "Laptop"
+'Rashmi' + 'Rashmi'
+      
+'RashmiRashmi'
+10 * 'Rashmi'
+      
+'RashmiRashmiRashmiRashmiRashmiRashmiRashmiRashmiRashmiRashmi'
+print ('c:\docs\rashmi')
+      
+c:\docs
+ashmi
+print (r'c:\docs\rashmi')
+      
+c:\docs\rashmi
+
+
+>>> 
+... Variables-
+      
+>>> SyntaxError: invalid syntax
+... x = 2
+>>>       
+... x + 3
+      
+>>> 5
+... y = 3
+>>>       
+... X + y
+      
+Traceback (most recent call last):
+  File "<pyshell#41>", line 1, in <module>
+    X + y
+>>> NameError: name 'X' is not defined. Did you mean: 'x'?
+... x + y
+      
+>>> 5
+... x + 9
+      
+>>> 11
+... x = 9
+>>>       
+... x + y
+      
+>>> 12
+... x
+      
+>>> 9
+... abc
+      
+Traceback (most recent call last):
+  File "<pyshell#47>", line 1, in <module>
+    abc
+>>> NameError: name 'abc' is not defined. Did you mean: 'abs'? Or did you forget to import 'abc'?
+... x + 10
+      
